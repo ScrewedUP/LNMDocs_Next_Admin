a6b47c69e3cdd3e6a704d70ed51bd30fbfe0e2a7
Sujal Jain <ethicalwolf333@gmail.com>
2023-06-15 00:06:05 +0530

    Moved the component folder to app directory

---
+++ app/components/Login.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/app/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/app/components/ui/card";
+import { Input } from "@/app/components/ui/input";
+import { Label } from "@/app/components/ui/label";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/app/components/ui/tabs";
+
+const Login = () => {
+  return <div>Login</div>;
+};
+
+export default Login;
