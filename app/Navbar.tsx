46ab25c319043bdfd1c5551a19ff2d837b5f39e2
Sujal Jain <ethicalwolf333@gmail.com>
2023-06-19 22:31:22 +0530

    Added the Navbar UI and Sign Out functionality

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -2,22 +2,58 @@
 
 import { useMediaQuery } from "@/hooks/useMediaQuery";
 import { signOut, useSession } from "next-auth/react";
+import { Poppins } from "next/font/google";
 import Link from "next/link";
+
+const poppins = Poppins({ weight: ["400", "600"], subsets: ["latin"] });
 
 const Navbar = () => {
   const session = useSession();
   const isLoggedIn = session.status === "authenticated";
   const isMobile = useMediaQuery("(max-width:800px)");
+  const navElements = isLoggedIn ? (
+    <>
+      <button
+        className='text-red-300 hover:text-red-400'
+        onClick={() => {
+          signOut({ callbackUrl: "/" });
+        }}
+      >
+        SignOut
+      </button>
+      <Link href='https://lnmdocs.vercel.app/' target='__blank'>
+        <div className='hover:text-violet-100 font-semibold text-lg hover:bg-violet-400 py-1 px-3 rounded hover:bg-transparent text-violet-400'>
+          LNMDocs
+        </div>
+      </Link>
+    </>
+  ) : (
+    <>
+      <Link href={`/login`}>
+        <div className=' text-lg hover:text-white text-gray-400'>Login</div>
+      </Link>
+      <Link href={`/register`}>
+        <div className=' text-lg hover:text-white text-gray-400'>Register</div>
+      </Link>
+      <Link href='https://lnmdocs.vercel.app/' target='__blank'>
+        <div className='hover:text-violet-100 font-semibold text-lg hover:bg-violet-400 py-1 px-3 rounded hover:bg-transparent text-violet-400'>
+          LNMDocs
+        </div>
+      </Link>
+    </>
+  );
   return (
     <div
-      className={`h-16 w-full flex ${
-        isMobile ? "flex-col justify-center h-32" : "flex-row justify-evenly"
+      className={`h-16 w-full ${poppins.className} flex ${
+        isMobile
+          ? "flex-col justify-center h-32"
+          : " flex-row justify-between  mt-6 fixed"
       } items-center text-white `}
     >
       <div
-        className={`${
-          isMobile ? "w-full text-center pb-4 " : "pl-20 w-1/2"
-        }  text-2xl font-mono`}
+        className={` ${
+          isMobile ? "w-full  pb-4" : "w-1/2"
+        } text-2xl text-center font-semibold`}
       >
         LNMDocs Admin
       </div>
@@ -26,29 +62,10 @@ const Navbar = () => {
         className={`flex ${
           isMobile
             ? "w-full justify-around border-2 p-4"
-            : "pr-40 w-full flex-1 justify-evenly"
+            : "w-1/2 flex-1 gap-20 items-center justify-center"
         }`}
       >
-        <Link href={`/login`}>
-          <div className='font-mono text-lg text-gray-400'>Login</div>
-        </Link>
-        <Link href={`/register`}>
-          <div className='font-mono text-lg text-gray-400'>Register</div>
-        </Link>
-        {isLoggedIn && (
-          <button
-            onClick={() => {
-              signOut({ callbackUrl: "/" });
-            }}
-          >
-            SignOut
-          </button>
-        )}
-        <Link href='https://lnmdocs.vercel.app/' target='__blank'>
-          <div className='text-black font-mono text-lg bg-green-500 p-1 rounded'>
-            LNMDocs
-          </div>
-        </Link>
+        {navElements}
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
+// This is the / route.
+
 "use client";
 
 import Login from "./(auth)/login/Login";
 import { useSession } from "next-auth/react";
+import { useEffect } from "react";
 
 export default function HomePage() {
   const session = useSession();
   console.log(session);
   const isLoggedIn = session.status === "authenticated";
-  //session data retrieved from the server side
+
   return (
-    <div className='h-screen w-screen bg-dark flex justify-start items-center flex-col gap-9'>
-      {/* <h1 className={` font-bold text-6xl text-white mt-10 `}>LNMDocs Admin</h1> */}
-      {isLoggedIn && (
-        <div className='text-white text-4xl'>You are logged in</div>
-      )}
-      {<Login /> && !isLoggedIn}
+    <div className='h-screen w-full bg-dark flex justify-center items-center flex-col gap-9'>
+      {!isLoggedIn && <Login />}
+      {isLoggedIn && <div>Logged In</div>}
     </div>
   );
 }
